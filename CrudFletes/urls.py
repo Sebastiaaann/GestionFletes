8f4e0a82e3ca22ccf7d25a849cc220f2bae6b928
Sebastiaaann <sebastian.almonacid06@inacapmail.cl>
2023-11-26 17:47:53 -0300

    Primer Commit

---
+++ CrudFletes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('registarVehiculo/', views.registarVehiculo),
+    path('delVehiculo/<codigo>', views.delVehiculo, name="delVehiculo"),
+    path('edicionVehiculo/<codigo>', views.edicionVehiculo, name="edicionVehiculo"),
+    path('editarVehiculo/', views.editarVehiculo, name="editarVehiculo")
+]

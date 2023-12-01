827fe89e8eabdf21ea08920b70e74a5a5d8f5c16
Pablo Cardenas A <pablo.cardenas30@inacapmail.cl>
2023-12-01 03:27:17 -0300

    pequeño fix

--- CrudMantenciones/forms.py
+++ CrudMantenciones/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.forms.widgets import DateInput
 from .models import Mantenciones
 from datetime import date
 
@@ -10,6 +9,4 @@ class MantencionForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super(MantencionForm, self).__init__(*args, **kwargs)
-        self.fields['fechaInicio'].widget = DateInput(attrs={'type': 'date'}, format='%Y-%m-%d')
-        self.fields['fechaInicio'].widget.attrs['min'] = '2000-01-01'
         self.fields['fechaInicio'].widget.attrs['max'] = str(date.today())

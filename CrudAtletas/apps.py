e74589e643ded482af253981f08dff20b36bd861
PabloCardenasA <pablo.cardenas30@inacapmail.cl>
2023-12-23 07:57:42 -0300

    rehaciendo master

--- CrudAtletas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrudatletasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CrudAtletas'
--- Cuentas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from . import views
-
-urlpatterns = [
-    path('registro/', views.regitro, name='registro'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
-]

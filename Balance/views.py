bfe30b1e5132cda744c123f7b35ac33c41c61fb9
PabloCardenasA <pablo.cardenas30@inacapmail.cl>
2023-12-25 17:10:21 -0300

    terminando

--- Balance/views.py
+++ Balance/views.py
@@ -1,10 +1,29 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from CrudVehiculos.models import Vehiculos
+from django.db.models import Sum
+from IngresosEgresos.models import Ingresos, Egresos
 
+@login_required
 def balance(request):
     vehiculos_inactivos = Vehiculos.objects.filter(estado='inactivo').count()
     vehiculos_activos = Vehiculos.objects.filter(estado='activo').count()
+
+    ingresos = Ingresos.objects.all()
+    egresos = Egresos.objects.all()
+
+    total_ingresos = ingresos.aggregate(total=Sum('valor'))['total']
+    total_egresos = egresos.aggregate(total=Sum('valor'))['total']
+
+    total_ingresos = total_ingresos if total_ingresos is not None else 0
+    total_egresos = total_egresos if total_egresos is not None else 0
+
+    saldo_total = total_ingresos - total_egresos
+
     context = {
+        'total_ingresos': total_ingresos,
+        'total_egresos': total_egresos,
+        'saldo_total': saldo_total,
         'vehiculos_inactivos': vehiculos_inactivos,
         'vehiculos_activos': vehiculos_activos,
         }
